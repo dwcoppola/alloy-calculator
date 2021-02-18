4caae210c835c2208cc33ea775f8b0f179ca9ad4
David <66121421+dwcoppola@users.noreply.github.com>
2021-02-18 13:22:42 -0500

    Add files via upload

--- alloy.js
+++ alloy.js
@@ -5,7 +5,7 @@ P = G / (G + A)
 G = PA / (1 - P)
 */
 
-function process() {
+function processIndex() {
     alloy = document.getElementById('alloy');
     grain = document.getElementById('grain');
     percent = document.getElementById('percent');    
@@ -43,9 +43,61 @@ function clearFields() {
     grain.focus();
 }
 
-function pressEnter(event) {
+function pressEnterIndex(event) {
     if (event.which === 13 || event.keyCode === 13) {
-        process();
+        processIndex();
+    } else {
+    }
+}
+
+/* 
+The Math:
+NEED = ( (Target% * Total Weight) - Old Alloy Weight - New Alloy Weight ) / ( 1 - target )
+*/
+
+function processMixer() {
+    targetPercent = Number(document.getElementById('target-percent').value);
+    target = 1 - targetPercent;
+    newPercent = Number(document.getElementById('new-percent').value);
+    oldPercent = Number(document.getElementById('old-percent').value);
+    newWeight = Number(document.getElementById('new-weight').value);
+    oldWeight = Number(document.getElementById('old-weight').value);
+    newAlloyWeight = newWeight * (1 - newPercent);
+    oldAlloyWeight = oldWeight * (1 - oldPercent);
+    totalWeight = newWeight + oldWeight;
+    output = ((target * totalWeight) - newAlloyWeight - oldAlloyWeight) / (1 - target);
+    need = document.getElementById('need');
+    need.value = output.toFixed(2)
+}
+
+function clearMixerFields() {
+    newWeight = document.getElementById('new-weight');
+    newPercent = document.getElementById('new-percent');   
+    if (newPercent > 1) {
+        newPercent /= 100;
+    }
+    oldWeight = document.getElementById('old-weight');
+    oldPercent = document.getElementById('old-percent');
+    if (oldPercent > 1) {
+        oldPercent /= 100;
+    }
+    targetPercent = document.getElementById('target-percent');
+    if (targetPercent > 1) {
+        targetPercent /= 100;
+    }
+    need = document.getElementById('need');
+    newWeight.value = ""
+    newPercent.value = ""
+    oldWeight.value = ""
+    oldPercent.value = ""
+    targetPercent.value = ""
+    need.value = ""
+    newWeight.value = ""
+}
+
+function pressEnterMixer(event) {
+    if (event.which === 13 || event.keyCode === 13) {
+        processMixer();
     } else {
     }
 }
